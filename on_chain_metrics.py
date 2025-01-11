e564824f4a6771e969300ede06ad2b3f6bb80bd2
kaiki26 <kaiki@uixlabs.in>
2025-01-11 13:46:07 +0530

    Create on_chain_metrics.py

---
+++ on_chain_metrics.py
@@ -0,0 +1,8 @@
+def calculate_transaction_success_rate(successful_transactions, total_transactions):
+    return (successful_transactions / total_transactions) * 100
+
+def calculate_gas_efficiency(average_gas_cost, max_gas_cost):
+    return average_gas_cost / max_gas_cost
+
+def calculate_error_handling_rate(errors_handled, total_errors):
+    return (errors_handled / total_errors) * 100
